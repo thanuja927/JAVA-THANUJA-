b046b4aeddaece6c6b3fa7fef6898189c44951cb
Thanuja <thanujas0137>
2026-05-24 23:14:20 +0530

    updated code

--- Data.java
+++
@@ -1,6 +0,0 @@
-public class Data{
-    public static void main(String[] args){
-        long age = 15000000L;
-        System.out.println(age);
-    }
-}
--- type.java
+++
@@ -1,8 +0,0 @@
-public class type{
-    public static void main(String[] args){
-        //widening
-        int age = 10;
-        float Myage = age;
-        System.out.println(Myage);
-    }
-}
